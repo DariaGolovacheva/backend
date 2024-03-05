0f50467611400d31f299eb9500cef6deb3b597d5
DariaGolovacheva <144705786+DariaGolovacheva@users.noreply.github.com>
2024-03-05 20:27:38 +0300

    Update index.php

--- task2.2.2/index.php
+++ task2.2.2/index.php
@@ -10,6 +10,8 @@
     <img src="./2.3.PNG"></img>
     <img src="./2.4.PNG"></img>
     <img src="./2.5.PNG"></img>
+    <img src="./2.6.PNG"></img>
+    <img src="./2.7.PNG"></img>
 
 </body>
 </html>
