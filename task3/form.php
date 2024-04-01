fbb2af9d85663042ca39f23a4f663aeb8d912d27
DariaGolovacheva <144705786+DariaGolovacheva@users.noreply.github.com>
2024-04-02 00:02:49 +0300

    Update form.php

--- task3/form.php
+++ task3/form.php
@@ -1,47 +1,50 @@
+<form action="" method="POST">
+  <label for="fio">ФИО:</label>
+  <input type="text" name="fio" id="fio" required />
+  <br/>
 
-  <div class = "container" id = "form">
-    <p id="font-size3">Форма:</p>
-    <form action="обработчик.php" method="POST">
-      <label for="fio">1. ФИО:</label>
-      <input type="text" id="fio" name="fio"><br><br>
+  <label for="phone">Телефон:</label>
+  <input type="tel" name="phone" id="phone" required />
+  <br/>
 
-      <label for="phone">2. Телефон:</label>
-      <input type="tel" id="phone" name="phone" required><br><br>
+  <label for="email">Email:</label>
+  <input type="email" name="email" id="email" required />
+  <br/>
 
-      <label for="email">3. E-mail:</label>
-      <input type="text" id="email" name="email" required><br><br>
+  <label for="dob">Дата рождения:</label>
+  <input type="date" name="dob" id="dob" required />
+  <br/>
 
-      <label for="birthdate">4. Дата рождения:</label>
-      <input type="date" id="birthdate" name="birthdate" required><br><br>
+  <label for="gender">Пол:</label>
+  <select name="gender" id="gender" required>
+    <option value="male">Мужской</option>
+    <option value="female">Женский</option>
+  </select>
+  <br/>
 
-      <label>5 Пол:</label>
-      <input type="radio" id="male" name="gender" value="male" required>
-      <label for="male">Мужской</label>
-      <input type="radio" id="female" name="gender" value="female" required>
-      <label for="female">Женский</label><br><br>
+  <label for="languages">Языки программирования:</label>
+  <select name="languages[]" id="languages" multiple required>
+    <option value="1">Pascal</option>
+    <option value="2">C</option>
+    <option value="3">C++</option>
+    <option value="4">JavaScript</option>
+    <option value="5">PHP</option>
+    <option value="6">Python</option>
+    <option value="7">Java</option>
+    <option value="8">Haskel</option>
+    <option value="9">Clojure</option>
+    <option value="10">Prolog</option>
+    <option value="11">Scala</option>
+  </select>
+  <br/>
 
-      <label>6. Любимый язык программирования:</label><br>
-      <select name="programming_languages[]" multiple>
-        <option value="Pascal">Pascal</option>
-        <option value="C">C</option>
-        <option value="C++">C++</option>
-        <option value="JavaScript">JavaScript</option>
-        <option value="PHP">PHP</option>
-        <option value="Python">Python</option>
-        <option value="Java">Java</option>
-        <option value="Haskell">Haskell</option>
-        <option value="Clojure">Clojure</option>
-        <option value="Prolog">Prolog</option>
-        <option value="Scala">Scala</option>
-      </select><br><br>
-      <label for="bio">7. Биография:</label><br>
-      <textarea id="bio" name="bio" rows="4" required></textarea><br><br>
+  <label for="bio">Биография:</label><br/>
+  <textarea name="bio" id="bio" rows="4" required></textarea>
+  <br/>
 
-      <input type="checkbox" id="contract" name="contract" required>
-      <label for="contract">8. С контрактом ознакомлен(а)</label><br><br>
+  <label for="contract">С контрактом ознакомлен:</label>
+  <input type="checkbox" name="contract" id="contract" required />
+  <br/>
 
-  <input type="submit" value="ok" />
-    </form>
-  </div>
-</div>
-  
+  <input type="submit" value="Отправить" />
+</form>
