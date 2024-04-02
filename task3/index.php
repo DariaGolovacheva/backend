b0c4d1762c4b492455a13c71dba9af60b880f8f2
DariaGolovacheva <144705786+DariaGolovacheva@users.noreply.github.com>
2024-04-02 16:59:11 +0300

    Update index.php

--- task3/index.php
+++ task3/index.php
@@ -40,7 +40,7 @@ if (empty($_POST['name'])) {
     $errors[] = 'Заполните ФИО.';
 }elseif(!preg_match("/^[а-яА-яЁёя-zA-Z\s]+$/u",$_POST['name'])){
     $errors[]='ФИО должно содержать только буквы и пробелы';
-    }elseif (strlen($_POST['name']>150){
+    }elseif (strlen($_POST['name']>150)){
              $errors[]='ФИО не должно превышать 150 символов';
              }
              
