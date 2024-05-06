a8c3ecd4db7c2f74b39cc84a1b1e456b72c01385
DariaGolovacheva <144705786+DariaGolovacheva@users.noreply.github.com>
2024-05-06 19:57:07 +0300

    Update index.php

--- task3/index.php
+++ task3/index.php
@@ -13,88 +13,74 @@ function validate_phone($phone) {
 $errors = [];
 
 // Проверка метода запроса
-if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-    if (!empty($_GET['save'])) {
-        echo 'Спасибо, результаты сохранены.';
-    }
-    include('form.php');
-    exit();
-}
-
-// Валидация данных
-if (empty($_POST['name'])) {
-    $errors[] = 'Введите ФИО.';
-} elseif (!preg_match('/^[a-zA-Zа-яА-Я\s]{1,150}$/', $_POST['name'])) {
-    $errors[] = 'Неправильный формат ФИО.';
-}
-
-if (empty($_POST['email'])) {
-    $errors[] = 'Введите email.';
-} elseif (!validate_email($_POST['email'])) {
-    $errors[] = 'Неправильный формат email.';
-}
-
-if (empty($_POST['phone'])) {
-    $errors[] = 'Введите номер телефона.';
-} elseif (!validate_phone($_POST['phone'])) {
-    $errors[] = 'Неправильный формат номера телефона.';
-}
-
-if (empty($_POST['dob'])) {
-    $errors[] = 'Введите дату рождения.';
-} elseif (!preg_match('/^\d{4}-\d{2}-\d{2}$/', $_POST['dob'])) {
-    $errors[] = 'Неправильный формат даты рождения.';
-} else {
-    list($year, $month, $day) = explode('-', $_POST['dob']);
-    if (!checkdate($month, $day, $year)) {
-        $errors[] = 'Неправильная дата рождения.';
-    }
-}
-
-if (empty($_POST['gender'])) {
-    $errors[] = 'Укажите ваш пол.';
-}
-
-if (empty($_POST['bio'])) {
-    $errors[] = 'Введите вашу биографию.';
-}
-
-if (empty($_POST['abilities'])) {
-    $errors[] = 'Выберите хотя бы один навык.';
-}
-
-if (!empty($errors)) {
-    foreach ($errors as $error) {
-        echo $error . "<br>";
-    }
-    exit();
-}
-
-$user = 'u67498';
-$pass = '2427367';
-$dbname = 'u67498';
-$db = new PDO("mysql:host=localhost;dbname=$dbname", $user, $pass);
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-try {
-    // Вставка данных в таблицу applicationn
-    $stmt = $db->prepare("INSERT INTO applicationn (name, phone, email, dob, gender, bio, contract) VALUES (?, ?, ?, ?, ?, ?, ?)");
-    $stmt->execute([$_POST['name'], $_POST['phone'], $_POST['email'], $_POST['dob'], $_POST['gender'], $_POST['bio'], isset($_POST['contract']) ? 1 : 0]);
-
-    // Получение ID вставленной записи
-    $application_id = $db->lastInsertId();
-
-    // Вставка данных в таблицу application_ability
-    foreach ($_POST['abilities'] as $ability) {
-        $stmt = $db->prepare("INSERT INTO application_ability (application_id, language_id) VALUES (?, ?)");
-        $stmt->execute([$application_id, $ability]);
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // Валидация ФИО
+    if (empty($_POST['name'])) {
+        $errors[] = 'Введите ФИО.';
+    } elseif (!preg_match('/^[a-zA-Zа-яА-Я\s]{1,150}$/', $_POST['name'])) {
+        $errors[] = 'Неправильный формат ФИО.';
     }
 
-    // Перенаправление с сообщением об успешном сохранении
-    header('Location: ?save=1');
-} catch (PDOException $e) {
-    echo 'Ошибка выполнения запроса: ' . $e->getMessage();
-    // Здесь можно предоставить дополнительную информацию или инструкции для пользователя
-    exit();
+    // Валидация email
+    if (empty($_POST['email'])) {
+        $errors[] = 'Введите email.';
+    } elseif (!validate_email($_POST['email'])) {
+        $errors[] = 'Неправильный формат email.';
+    }
+
+    // Валидация телефона
+    if (empty($_POST['phone'])) {
+        $errors[] = 'Введите номер телефона.';
+    } elseif (!validate_phone($_POST['phone'])) {
+        $errors[] = 'Неправильный формат номера телефона.';
+    }
+
+    // Валидация даты рождения
+    if (empty($_POST['dob'])) {
+        $errors[] = 'Введите дату рождения.';
+    } elseif (!preg_match('/^\d{4}-\d{2}-\d{2}$/', $_POST['dob'])) {
+        $errors[] = 'Неправильный формат даты рождения.';
+    }
+
+    // Валидация пола
+    if (empty($_POST['gender'])) {
+        $errors[] = 'Укажите ваш пол.';
+    }
+
+    // Валидация биографии
+    if (empty($_POST['bio'])) {
+        $errors[] = 'Введите вашу биографию.';
+    }
+
+    // Валидация контракта
+    if (!isset($_POST['contract'])) {
+        $errors[] = 'Необходимо ознакомиться с контрактом.';
+    }
+
+    // Валидация выбранного языка программирования
+    if (empty($_POST['favoriteLanguage'])) {
+        $errors[] = 'Выберите хотя бы один язык программирования.';
+    }
+
+    if (empty($errors)) {
+        // Если ошибок нет, можно обрабатывать данные
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $phone = $_POST['phone'];
+        $dob = $_POST['dob'];
+        $gender = $_POST['gender'];
+        $bio = $_POST['bio'];
+        $contract = isset($_POST['contract']) ? 1 : 0;
+        $favoriteLanguages = $_POST['favoriteLanguage'];
+
+        // Далее можно добавить код для сохранения данных в базу данных или выполнять другие операции
+
+        echo 'Данные успешно обработаны и готовы к сохранению!';
+    } else {
+        // Если есть ошибки, вывести их пользователю
+        foreach ($errors as $error) {
+            echo $error . "<br>";
+        }
+    }
 }
 ?>
