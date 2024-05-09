8aff1f3eef7885d105ac328d6733c5289fcb9c54
DariaGolovacheva <144705786+DariaGolovacheva@users.noreply.github.com>
2024-05-09 19:15:59 +0300

    Update index.php

--- task4/index.php
+++ task4/index.php
@@ -95,6 +95,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Регистрационная форма</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
 
