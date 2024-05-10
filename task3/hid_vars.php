0c435a81da771a0039462609af62eda1b82b2bd9
DariaGolovacheva <144705786+DariaGolovacheva@users.noreply.github.com>
2024-05-10 18:58:21 +0300

    Delete task3 directory

--- task3/hid_vars.php
+++
@@ -1,6 +0,0 @@
-<?php
-$user = 'u67498'; // Заменить на ваш логин uXXXXX
-$pass = '2427367'; // Заменить на пароль
-$db = new PDO('mysql:host=localhost;dbname=u67498', $user, $pass,
-  [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-?>
