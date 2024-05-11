09c886f582773d2cb0fabc16e4b9212625ade518
DariaGolovacheva <144705786+DariaGolovacheva@users.noreply.github.com>
2024-05-12 01:40:22 +0300

    Update index.php

--- task3/index.php
+++ task3/index.php
@@ -1,157 +1,74 @@
 <?php
-// Отправляем браузеру правильную кодировку,
-// файл index.php должен быть в кодировке UTF-8 без BOM.
-header('Content-Type: text/html; charset=UTF-8');
-
-// В суперглобальном массиве $_SERVER PHP сохраняет некторые заголовки запроса HTTP
-// и другие сведения о клиненте и сервере, например метод текущего запроса $_SERVER['REQUEST_METHOD'].
-if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-    if (!empty($_GET['save'])) {
-        echo '<script>
-                    document.addEventListener("DOMContentLoaded", function() {
-                        var modal = document.createElement("div");
-                        modal.innerHTML = "Спасибо, результаты сохранены.";
-                        modal.style.position = "fixed";
-                        modal.style.zIndex = "1";
-                        modal.style.top = "0";
-                        modal.style.left = "0";
-                        modal.style.width = "100%";
-                        modal.style.height = "100%";
-                        modal.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
-                        modal.style.display = "flex";
-                        modal.style.justifyContent = "center";
-                        modal.style.alignItems = "center";
-                        modal.style.color = "#fff";
-                        modal.style.fontSize = "24px";
-                        modal.style.cursor = "pointer";
-                        modal.addEventListener("click", function() {
-                            modal.style.display = "none";
-                        });
-                        document.body.appendChild(modal);
-                    });
-              </script>';
-    }
-
-    include('form.php');
+function print_error($error)
+{
+    print($error);
     exit();
 }
-// Иначе, если запрос был методом POST, т.е. нужно проверить данные и сохранить их в XML-файл.
-
-// Проверяем ошибки.
-$errors = FALSE;
-if (empty($_POST['name'])) {
-    print('Заполните имя.<br/>');
-    $errors = TRUE;
-} elseif (!preg_match('/^[\p{L}\s]+$/u', $_POST['name'])) {
-    print('Имя может содержать только буквы и пробелы.<br/>');
-    $errors = TRUE;
-} elseif (strlen($_POST['name']) > 150) {
-    print('Имя не должно превышать 150 символов.<br/>');
-    $errors = TRUE;
-}
-
-if (empty($_POST['phone'])) {
-    print('Заполните телефон.<br/>');
-    $errors = TRUE;
-} elseif (!preg_match('/^\d+$/', $_POST['phone'])) {
-    print('Телефон должен состоять только из цифр.<br/>');
-    $errors = TRUE;
-} elseif (strlen($_POST['phone']) > 11) {
-    print('Телефон не должен превышать 11 символов.<br/>');
-    $errors = TRUE;
-}
-
-if (empty($_POST['data'])) {
-    print('Заполните дату.<br/>');
-    $errors = TRUE;
-} elseif (!preg_match('/^\d{4}-\d{2}-\d{2}$/', $_POST['data'])) {
-    print('Дата должна быть в формате YYYY-MM-DD.<br/>');
-    $errors = TRUE;
-}
-
-if (empty($_POST['email'])) {
-    print('Заполните адрес электронной почты.<br/>');
-    $errors = TRUE;
-} elseif (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-    print('Введите корректный адрес электронной почты.<br/>');
-    $errors = TRUE;
-} elseif (strlen($_POST['email']) > 150) {
-    print('Адрес электронной почты не должен превышать 150 символов.<br/>');
-    $errors = TRUE;
-}
-
-if (empty($_POST['sex'])) {
-    print('Выберите пол.<br/>');
-    $errors = TRUE;
-} elseif ($_POST['sex'] !== 'male' && $_POST['sex'] !== 'female') {
-    print('Выберите только мужской или женский пол.<br/>');
-    $errors = TRUE;
-}
-
-if (empty($_POST['ok'])) {
-    print('Подтвердите соглашение.<br/>');
-    $errors = TRUE;
-} elseif ($_POST['ok'] !== 'on') {
-    print('Подтвердите соглашение.<br/>');
-    $errors = TRUE;
-}
-
-$allowed_languages = array("Pascal", "C", "C++", "JavaScript", "PHP", "Python", "Java", "Haskel");
-
-if (empty($_POST['langs'])) {
-    print('Выберите хотя бы 1 язык программирования.<br/>');
-    $errors = TRUE;
-} else {
-    foreach ($_POST['langs'] as $language) {
-        if (!in_array($language, $allowed_languages)) {
-            print('Выберите только представленные языки.<br/>');
-            $errors = TRUE;
-            break;
+function validate_data($data)
+{
+    $all_names = ["fio", "telephone", "email", "bday", "sex", "langs", "biography", "contract"];
+    $size_limits = ["fio" => 255, "email" => 255, "biography" => 512];
+    foreach ($all_names as $key) {
+        if (empty($data[$key])) {
+            print_error("Field " . $key . " is empty.");
+        } elseif (in_array($key, array_keys($size_limits))
+            && strlen($data[$key]) > $size_limits[$key]) {
+            print_error("Length of the contents of the field " . $key . " more than " . $size_limits[$key]
+                . " symbols.");
+        } elseif ($key == "telephone" && !preg_match('/^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?$/', $data[$key])) {
+            print_error("Invalid telephone.");
         }
     }
 }
 
-if (empty($_POST['biography'])) {
-    print('Заполните биографию.<br/>');
-    $errors = TRUE;
-} elseif (strlen($_POST['biography']) > 300) {
-    print('Биография не должна превышать 300 символов.<br/>');
-    $errors = TRUE;
-}
+function save_to_database($data)
+{
+    include("../hid_vars.php");
+    $db_req = 'mysql:dbname=' . $database . ';host=' . $host;
+    try {
+        $db = new PDO($db_req, $user, $password,
+            [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+        $names_data_for_app = ['fio', 'telephone', 'email', 'bday', 'sex', 'biography'];
+        $app_req = "INSERT INTO application (" . implode(', ', $names_data_for_app) .
+            ") VALUES (";
+        $data_for_app = [];
+        foreach ($names_data_for_app as $name) {
+            $data_for_app[] = "'" . $data[$name] . "'";
+        }
+        $app_req = $app_req . implode(', ', $data_for_app) . ");";
+        $app_stmt = $db->prepare($app_req);
+        $app_stmt->execute();
 
-if (empty($_POST['contract'])) {
-    print('С контрактом ознакомлен (а).<br/>');
-    $errors = TRUE;
-}
-
-if ($errors) {
-    // При наличии ошибок завершаем работу скрипта.
-    exit();
-}
-
-$user = 'u67498'; // Заменить на ваш логин uXXXXX
-$pass = '2427367'; // Заменить на пароль
-$db = new PDO('mysql:host=localhost;dbname=u67498', $user, $pass,
-    [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-
-// Подготовленный запрос. Не именованные метки.
-try {
-    $stmt = $db->prepare("INSERT INTO application (name, phone, email, data, pol, bio, ok) VALUES (?, ?, ?, ?, ?, ?, ?)");
-    $stmt->execute([$_POST['name'], $_POST['phone'], $_POST['email'], $_POST['data'], $_POST['sex'], $_POST['biography'], $_POST['ok']]);
-    $lastId = $db->lastInsertId();
-
-    foreach ($_POST['langs'] as $lang) {
-        $stmtLang = $db->prepare("SELECT id FROM language WHERE name = ?");
-        $stmtLang->execute([$lang]);
-        $languageId = $stmtLang->fetchColumn();
-
-        $stmtApLang = $db->prepare("INSERT INTO ap_lan (id_application, id_language) VALUES (?, ?)");
-        $stmtApLang->execute([$lastId, $languageId]);
+        $last_app_id = $db->lastInsertId();
+        $link_req = "INSERT INTO app_link_lang (id_app, id_prog_lang) VALUES ";
+        $data_for_link = [];
+        foreach ($data["langs"] as $lang) {
+            $data_for_link[] = "(" . $last_app_id . ", " . $lang . ")";
+        }
+        $link_req = $link_req . implode(", ", $data_for_link) . ";";
+        $link_stmt = $db->prepare($link_req);
+        $link_stmt->execute();
+        print("Data successfully saved to the database.");
+    } catch (PDOException $e) {
+        print_error($e->getMessage());
     }
-} catch (PDOException $e) {
-    print('Error : ' . $e->getMessage());
-    exit();
 }
 
-// Делаем перенаправление.
-header('Location: ?save=1');
+
+header('Content-Type: text/html; charset=UTF-8');
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    if (!empty($_GET['save'])) {
+
+        print('The data is stored in the database.');
+    }
+    include('body.php');
+    exit();
+}
+$form_data = $_POST;
+//foreach ($form_data as $key => $val)
+//{
+//    print($key . " = '" . $val . "' empty = " . isset($val) . " ");
+//}
+validate_data($form_data);
+save_to_database($form_data);
+
