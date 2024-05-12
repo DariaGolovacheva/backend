1589a2aaa69bc288d6850baabcdd74aac0eb6d34
DariaGolovacheva <144705786+DariaGolovacheva@users.noreply.github.com>
2024-05-12 20:18:27 +0300

    Update index.php

--- task4/index.php
+++ task4/index.php
@@ -73,9 +73,6 @@ function save_to_database($data)
 }
 
 
-if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-    include('form.php');
-}
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $all_names = ["name", "phone", "email", "dob", "gender", "favourite_language", "bio", "contract"];
     $form_data = array_fill_keys($all_names, "");
@@ -95,3 +92,66 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     header("Location:" . parse_url($_SERVER['REQUEST_URI'])['path'] . "?success_flag=true");
 }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Регистрационная форма</title>
+<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+
+<div class="container">
+  <h2>Регистрационная форма</h2>
+  <form id="registrationForm" method="POST" action="">
+    <div class="form-group">
+      <label for="name">ФИО:</label>
+      <input type="text" id="fullName" name="name" >
+    </div>
+    <div class="form-group">
+      <label for="phone">Телефон:</label>
+      <input type="tel" id="phone" name="phone">
+    </div>
+    <div class="form-group">
+      <label for="email">E-mail:</label>
+      <input type="email" id="email" name="email" >
+    </div>
+    <div class="form-group">
+      <label for="dob">Дата рождения:</label>
+      <input type="date" id="dob" name="dob" >
+    </div>
+    <div class="form-group">
+      <label>Пол:</label>
+      <label><input type="radio" name="gender" value="male" checked> Мужской</label>
+      <label><input type="radio" name="gender" value="female"> Женский</label>
+    </div>
+    <div class="form-group">
+      <label for="favoriteLanguage">Любимый язык программирования:</label>
+      <select id="favoriteLanguage" name="favoriteLanguage[]" multiple >
+        <option value="Pascal">Pascal</option>
+        <option value="C">C</option>
+        <option value="C++">C++</option>
+        <option value="JavaScript">JavaScript</option>
+        <option value="PHP">PHP</option>
+        <option value="Python">Python</option>
+        <option value="Java">Java</option>
+        <option value="Haskel">Haskel</option>
+        <option value="Clojure">Clojure</option>
+        <option value="Prolog">Prolog</option>
+        <option value="Scala">Scala</option>
+      </select>
+    </div>
+    <div class="form-group">
+      <label for="bio">Биография:</label>
+      <textarea id="bio" name="bio" rows="5" ></textarea>
+    </div>
+    <div class="form-group">
+      <label><input type="checkbox" id="contract" name="contract" > С контрактом ознакомлен (а)</label>
+    </div>
+    <button type="submit">Сохранить</button>
+  </form>
+</div>
+
+</body>
+</html>
