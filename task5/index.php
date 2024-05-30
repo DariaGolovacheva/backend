009f76f717654305bbc1f09d214b6a3ace39a327
DariaGolovacheva <144705786+DariaGolovacheva@users.noreply.github.com>
2024-05-30 22:22:48 +0300

    Update index.php

--- task5/index.php
+++ task5/index.php
@@ -8,7 +8,9 @@
 // Отправляем браузеру правильную кодировку,
 // файл index.php должен быть в кодировке UTF-8 без BOM.
 header('Content-Type: text/html; charset=UTF-8');
-
+define("user", "u67498");
+define("password", "2427367");
+define("dbname", "u67498");
 // В суперглобальном массиве $_SERVER PHP сохраняет некторые заголовки запроса HTTP
 // и другие сведения о клиненте и сервере, например метод текущего запроса $_SERVER['REQUEST_METHOD'].
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
