2f439736018deeef17b14d14068c4657b2746930
DariaGolovacheva <144705786+DariaGolovacheva@users.noreply.github.com>
2024-05-31 18:37:24 +0300

    Update login.php

--- task6/login.php
+++ task6/login.php
@@ -1,9 +1,48 @@
+<html>
+  <head>
+    <title>Вход</title>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
+<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
+    <style>
+/* Сообщения об ошибках и поля с ошибками выводим с красным бордюром. */
+.error {
+  border: 2px solid red;
+}
+      form {
+  max-width: 300px;
+  margin: 0 auto;
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  background-color: #f9f9f9;
+}
+
+input {
+  width: 100%;
+  padding: 10px;
+  margin: 5px 0;
+  border: 1px solid #ccc;
+  border-radius: 3px;
+}
+
+input[type="submit"] {
+  background-color: #1c87c9;
+  color: white;
+  border: none;
+  border-radius: 3px;
+  padding: 10px 20px;
+  cursor: pointer;
+}
+
+input[type="submit"]:hover {
+  background-color: #45a049;
+}
+.error {
+  border: 1px solid red;
+}
+    </style>
+  </head>
 <?php
-
-define("user", "u67498");
-define("password", "2427367");
-define("dbname", "u67498");
-
 /**
  * Файл login.php для не авторизованного пользователя выводит форму логина.
  * При отправке формы проверяет логин/пароль и создает сессию,
@@ -11,7 +50,7 @@ define("dbname", "u67498");
  * После авторизации пользователь перенаправляется на главную страницу
  * для изменения ранее введенных данных.
  **/
-
+require_once('db.php');   
 // Отправляем браузеру правильную кодировку,
 // файл login.php должен быть в кодировке UTF-8 без BOM.
 header('Content-Type: text/html; charset=UTF-8');
@@ -19,94 +58,109 @@ header('Content-Type: text/html; charset=UTF-8');
 // В суперглобальном массиве $_SESSION хранятся переменные сессии.
 // Будем сохранять туда логин после успешной авторизации.
 $session_started = false;
-if (isset($_COOKIE[session_name()]) && session_start()) {
-  $session_started = true;
-  if (!empty($_SESSION['login'])) {
-    // Если есть логин в сессии, то пользователь уже авторизован.
-    // TODO: Сделать выход (окончание сессии вызовом session_destroy()
-    //при нажатии на кнопку Выход).
-    if (isset($_POST['logout'])) {
-        session_destroy();
-        echo "Вы вышли из сессии.";
+if (session_start() && !empty($_COOKIE[session_name()])) {
+    $session_started = true;
+    if (!empty($_SESSION['login'])) {
+        // Если есть логин в сессии, то пользователь уже авторизован.
+        if (!empty($_COOKIE['logout'])) {
+            setcookie('logout', '', 100000);
+            // Выход пользователя из сессии
+            session_destroy();
+        }
+
+        // Делаем перенаправление на форму.
         header('Location: ./');
         exit();
     }
-
-    // Делаем перенаправление на форму.
-    header('Location: ./');
-    exit();
-  }
 }
 
 // В суперглобальном массиве $_SERVER PHP сохраняет некторые заголовки запроса HTTP
 // и другие сведения о клиненте и сервере, например метод текущего запроса $_SERVER['REQUEST_METHOD'].
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    $errors = array();
+      $messages = array();
+  $errors['error'] = !empty($_COOKIE['error']);
+
+  // TODO: аналогично все поля.
+
+  // Выдаем сообщения об ошибках.
+  if (!empty($errors['error'])) {
+    setcookie('error', '', 100000);
+    // Выводим сообщение.
+    $messages[] = '<div class="error">Неправильный логин или пароль</div>';
+  }
+  // TODO: тут выдать сообщения об ошибках в других полях.
+  // Складываем предыдущие значения полей в массив, если есть.
+  // При этом санитизуем все данные для безопасного отображения в браузере.
+  $values = array();
+  $values['login'] = empty($_COOKIE['login_value']) ? '' : strip_tags($_COOKIE['login_value']);
+  $values['password'] = empty($_COOKIE['password_value']) ? '' : strip_tags($_COOKIE['password_value']);
+
+// Далее выводим форму отмечая элементы с ошибками классом error
+// и задавая начальные значения элементов ранее сохраненными.
 ?>
-
-<body style="display: flex; flex-direction: column; justify-content: center; align-items: center">
-
-<h1>
-  Login
-</h1>
-
+<body>
 <form action="" method="post">
-  <input name="login" />
-  <input name="pass" />
-  <input type="submit" value="Войти" />
+  <?php 
+  if (!empty($messages)) {
+  print('<div id="messages">');
+  // Выводим все сообщения.
+  foreach ($messages as $message) {
+    print($message);
+  }
+  print('</div>');
+}
+    ?>
+  <input name="login" <?php if ($errors['error']) {print 'class="error"';} ?> value="<?php print $values['login']; ?>"/>
+  <input name="password" <?php if ($errors['error']) {print 'class="error"';} ?> value="<?php print $values['password']; ?>"/>
+  <input type="submit" name = "button" value="Войти" />
+  <input type="submit" name = "button" value="Вход для админа" />
 </form>
-
-<input type="submit" name = "logout" value="Выйти" />
-
-</body>
-
-
+  </body>
+</html>
 
 <?php
 }
+else
+{
+    if($_POST['button'] == 'Войти')
+  {
 // Иначе, если запрос был методом POST, т.е. нужно сделать авторизацию с записью логина в сессию.
-else {
-      if (!$session_started) {
-        session_start();
-      }
 
-      $user = user;
-      $pass = password;
-      $db = new PDO('mysql:host=localhost;dbname=' . dbname, $user, $pass, [
-        PDO::ATTR_PERSISTENT => true,
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-      ]);
-
-      try {
-        // Проверяем, есть ли пользователь с таким логином и паролем
-        $login = $_POST['login'];
-        $password = $_POST['pass'];
-        $md5Pass = md5($password);
-
-        $stmt = $db->prepare("SELECT personId FROM personAuthentificationData WHERE login = :login AND pass = :pass");
-        $stmt->execute([':login' => $login, ':pass' => $md5Pass]);
-        $authData = $stmt->fetch(PDO::FETCH_ASSOC);
-
-        if ($authData) {
-          // Пользователь существует, сохраняем данные в сессию
-          $_SESSION['login'] = $login;
-          $_SESSION['uid'] = $authData['personId'];
-
-          // Делаем перенаправление на главную страницу
-          header('Location: ./');
-          exit();
-        } else {
-          // Пользователь не найден, выдаем ошибку
-          echo "Неверный логин или пароль.";
-          echo $md5Pass;
-          exit();
-        }
-      } catch(PDOException $e){
-        print('Error : ' . $e->getMessage());
+  // TODO: Проверть есть ли такой логин и пароль в базе данных.
+  // Выдать сообщение об ошибках.
+    $login = $_POST['login'];
+    $password = $_POST['password'];
+    $use = db_get_Pass_Login_user($login);
+    
+    // Сохраняем ранее введенное в форму значение на месяц.
+    setcookie('login_value', $_POST['login'], time() + 30 * 24 * 60 * 60);
+    setcookie('password_value', $_POST['password'], time() + 30 * 24 * 60 * 60);
+  
+    // Проверка наличия пользователя и совпадения пароля
+    if ($use && password_verify($password, $use['password'])) {
+        // Логин и пароль верные
+   setcookie('login_value', '', 100000);
+    setcookie('password_value', '', 100000);
+    } else {
+        // Логин или пароль неверные
+     setcookie('error', '1', time() + 24 * 60 * 60);
+      header('Location: login.php');
         exit();
-      }
-
-
+    }
+  if (!$session_started) {
+    session_start();
+  }
+  // Если все ок, то авторизуем пользователя.
+  $_SESSION['login'] = $login;
+  // Записываем ID пользователя.
+    $_SESSION['uid'] = $use['userid'];
 
   // Делаем перенаправление.
   header('Location: ./');
+  }
+    else{
+        header('Location: admin.php');
+      exit();
+    }
 }
