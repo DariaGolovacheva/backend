0c5ccee616b6b8acd6254d0f53bc11211a05f5a3
DariaGolovacheva <144705786+DariaGolovacheva@users.noreply.github.com>
2024-06-02 19:47:08 +0300

    Update form.php

--- task4/form.php
+++ task4/form.php
@@ -1,113 +1,137 @@
 <html>
 <head>
 <title>Задание №4</title>
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
-<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
+
 <style>
   /* Сообщения об ошибках и поля с ошибками выводим с красным бордюром. */
 .error {
   border: 2px solid red !important;
 }
 html, body {
-min-height: 100%;
-padding: 0;
-margin: 0;
-font-family: Roboto, Arial, sans-serif;
-font-size: 14px;
-color: #666;
-}
-h1 {
-margin: 0 0 20px;
-font-weight: 400;
-color: #1c87c9;
-}
-p {
-margin: 0 0 5px;
-}
-.main-block {
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
-min-height: 100vh;
-background: #1c87c9;
-}
-form {
-padding: 25px;
-margin: 25px;
-box-shadow: 0 2px 5px #f5f5f5; 
-background: #f5f5f5; 
-}
-.fas {
-margin: 25px 10px 0;
-font-size: 72px;
-color: #fff;
-}
-.fa-envelope {
-transform: rotate(-20deg);
-}
-.fa-at , .fa-mail-bulk{
-transform: rotate(10deg);
-}
-.f {
-width: calc(100% - 18px);
-padding: 8px;
-margin-bottom: 20px;
-border: 1px solid #1c87c9;
-outline: none;
-}
-input::placeholder {
-color: #666;
-}
-button {
-width: 100%;
-padding: 10px;
-border: none;
-background: #1c87c9; 
-font-size: 16px;
-font-weight: 400;
-color: #fff;
-}
-button:hover {
-background: #2371a0;
-} 
-@media (min-width: 1300px) {
-.main-block {
-flex-direction: row;
-}
-.left-part, form {
-width: 50%;
-}
-.fa-envelope {
-margin-top: 0;
-margin-left: 20%;
-}
-.fa-at {
-margin-top: -10%;
-margin-left: 65%;
-}
-.fa-mail-bulk {
-margin-top: 2%;
-margin-left: 28%;
-}) {
-.main-block {
-flex-direction: row;
-}
-.left-part, form {
-width: 50%;
-}
-.fa-envelope {
-margin-top: 0;
-margin-left: 20%;
-}
-.fa-at {
-margin-top: -10%;
-margin-left: 65%;
-}
-.fa-mail-bulk {
-margin-top: 2%;
-margin-left: 28%;
-}
+      min-height: 100%;
+      padding: 0;
+      margin: 0;
+      font-family: Roboto, Arial, sans-serif;
+      font-size: 14px;
+      color: #fff;
+      background-color: #000;
+    }
+
+    h1 {
+      margin: 0 0 20px;
+      font-weight: 400;
+      color: #FFA500;
+    }
+
+    p {
+      margin: 0 0 5px;
+    }
+
+    .main-block {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      min-height: 100vh;
+      background: #000;
+    }
+
+    form {
+      padding: 25px;
+      margin: 25px;
+      box-shadow: 0 2px 5px #FFA500;
+      background: #222;
+    }
+
+    .fas {
+      margin: 25px 10px 0;
+      font-size: 72px;
+      color: #FFA500;
+    }
+
+    .fa-envelope {
+      transform: rotate(-20deg);
+    }
+
+    .fa-at, .fa-mail-bulk {
+      transform: rotate(10deg);
+    }
+
+    .f {
+      width: calc(100% - 18px);
+      padding: 8px;
+      margin-bottom: 20px;
+      border: 1px solid #FFA500;
+      outline: none;
+    }
+
+    input::placeholder {
+      color: #fff;
+    }
+
+    button {
+      width: 100%;
+      padding: 10px;
+      border: none;
+      background: #FFA500;
+      font-size: 16px;
+      font-weight: 400;
+      color: #fff;
+    }
+
+    button:hover {
+      background: #D48100;
+    }
+
+    table {
+      width: 100%;
+      border-collapse: collapse;
+      margin: 20px 0;
+    }
+
+    table, th, td {
+      border: 1px solid #FFA500;
+    }
+
+    th, td {
+      padding: 10px;
+      text-align: left;
+      background: #333;
+    }
+
+    th {
+      background: #444;
+    }
+
+    table {
+      border-radius: 10px;
+      overflow: hidden;
+    }
+
+    @media (min-width: 1300px) {
+      .main-block {
+        flex-direction: row;
+      }
+
+      .left-part, form {
+        width: 50%;
+      }
+
+      .fa-envelope {
+        margin-top: 0;
+        margin-left: 20%;
+      }
+
+      .fa-at {
+        margin-top: -10%;
+        margin-left: 65%;
+      }
+
+      .fa-mail-bulk {
+        margin-top: 2%;
+        margin-left: 28%;
+      }
+    }
 </style>
 </head>
 <body>
@@ -120,7 +144,7 @@ margin-left: 28%;
 <form action="" method="POST">
 <h1 style="display: flex;
 justify-content: center;
-align-items: center;">Заявка</h1>
+align-items: center;">Регистрационная форма</h1>
       <?php
 if (!empty($messages)) {
   print('<div id="messages">');
